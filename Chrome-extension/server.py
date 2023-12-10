4e81252f398471a3b5aca5e189d2bccf4cb7ea8c
Chris Johnson <christopher.johnson13@ontariotechu.net>
2023-12-10 00:45:22 -0500

    Extension functionally finished
    There is still some styling to do (maybe), and the LSTM model does not function properly.
    
    I will upload the lightning_logs to the remote once/if I figure out how to fix the LSTM model.

--- Chrome-extension/server.py
+++ Chrome-extension/server.py
@@ -1,236 +1,89 @@
-from flask import Flask, Response
-from lightning import LightningModule
+# pandas
+import pandas as pd
+
+# classifers (from local python file)
+from models import RNN_classifier, GRU_classifier, LSTM_classifier
+
+# torch
 import torch
-import torch.nn as nn
-from torchmetrics import Accuracy
+from torchtext.vocab import Vocab
 
-# TODO: Import models from lightning_logs
-# RNN_model = LightningModule.load_from_checkpoint('lightning_logs/RNN/version_0/checkpoints/epoch=19-step=26020.ckpt', strict=False)
-# GRU_model = LightningModule.load_from_checkpoint('lightning_logs/GRU/version_0/checkpoints/epoch=19-step=26020.ckpt', strict=False)
-# LSTM_model = LightningModule.load_from_checkpoint('lightning_logs/LSTM/version_0/checkpoints/epoch=19-step=26020.ckpt', strict=False)
+# nltk (tokenizer)
+from nltk import word_tokenize
 
-# a basic RNN classifier using a LightningModule
-class RNN_classifier(LightningModule):
-    def __init__(self, vocab_size, embedding_dimension, state_dimension):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dimension)
-        self.rnn = nn.RNN(
-            input_size = embedding_dimension,
-            hidden_size = state_dimension,
-            num_layers = 1, # hyperparameter
-            batch_first = True
-        )
+# tqdm (progress bar)
+from tqdm.notebook import tqdm
 
-        # possible outputs: positive, negative, neutral
-        self.output = nn.Linear(state_dimension, 3)
+# flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-        # activation function to provide probabilities
-        self.activation = nn.Softmax(dim = 1)
+# import vocab from disk
+vocab = torch.load('vocab.pth')
 
-        # monitors accuracy
-        self.accuracy = Accuracy(task = 'multiclass', num_classes = 3)
+#! IMPORTANT: This server WILL NOT FUNCTION PROPERLY if the models have not 
+#! already been trained!
+# load & configure the RNN model
+RNN_model = RNN_classifier(
+    vocab_size = len(vocab),
+    embedding_dimension = 32, # hyperparameter
+    state_dimension = 64 # hyperparameter
+)
+RNN_model.load_state_dict(torch.load('lightning_logs/RNN/version_0/checkpoints/epoch=19-step=26020.ckpt')['state_dict'])
+RNN_model.eval()
 
-    def forward(self, sequence_batch):
-        embedded = self.embedding(sequence_batch)
-        h_t, h_n = self.rnn(embedded)  # output features (h_t) and state (h_n)
-        output = self.output(h_n[-1])
-        output = self.activation(output)
+# load & configure the GRU model
+GRU_model = GRU_classifier(
+    vocab_size = len(vocab),
+    embedding_dimension = 512, # hyperparameter
+)
+GRU_model.load_state_dict(torch.load('lightning_logs/GRU/version_0/checkpoints/epoch=19-step=26020.ckpt')['state_dict'])
+GRU_model.eval()
 
-        return output
-
-    def loss(self, output, targets):
-        loss = nn.CrossEntropyLoss()
-
-        return loss(output, targets)
-
-    def training_step(self, batch):
-        inputs, targets = batch
-        outputs = self.forward(inputs)
-        loss = self.loss(outputs, targets)
-
-        # get accuracy value
-        self.accuracy(outputs, targets)
-
-        # log the training accuracy
-        self.log('training accuracy', self.accuracy, prog_bar = True)
-
-        return loss
-
-    def validation_step(self, batch):
-        inputs, targets = batch
-        outputs = self.forward(inputs)
-
-        # get accuracy value
-        self.accuracy(outputs, targets)
-
-        # log the validation accuracy
-        self.log('validation accuracy', self.accuracy, prog_bar = True)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters())
-
-class GRU_classifier(LightningModule):
-    def __init__(self, vocab_size, embedding_dimension):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dimension)
-        self.GRU = nn.GRU(input_size = embedding_dimension, hidden_size = 100, batch_first=True)
-        self.linear1 = nn.Linear(100, 62)
-        self.ReLU = nn.ReLU()
-        self.linear2 = nn.Linear(62, 32)
-
-        # possible outputs: positive, negative, neutral
-        self.output = nn.Linear(32, 3)
-
-        # activation function to provide probabilities
-        self.activation = nn.Softmax(dim = 1)
-
-        # monitors accuracy
-        self.accuracy = Accuracy(task = 'multiclass', num_classes = 3)
-
-    def forward(self, sequence_batch):
-        output = self.embedding(sequence_batch)
-        output, h_o = self.GRU(output)
-        output = self.linear1(h_o[-1])
-        output = self.ReLU(output)
-        output = self.linear2(output)
-        output = self.ReLU(output)
-        output = self.output(output)
-        output = self.activation(output)
-
-        return output
-
-    def loss(self, output, targets):
-        loss = nn.CrossEntropyLoss()
-
-        return loss(output, targets)
-
-    def training_step(self, batch):
-        inputs, targets = batch
-        outputs = self.forward(inputs)
-        loss = self.loss(outputs, targets)
-
-        # get accuracy value
-        self.accuracy(outputs, targets)
-
-        # log the training accuracy
-        self.log('training accuracy', self.accuracy, prog_bar = True)
-
-        return loss
-
-    def validation_step(self, batch):
-        inputs, targets = batch
-        outputs = self.forward(inputs)
-
-        # get accuracy value
-        self.accuracy(outputs, targets)
-
-        # log the validation accuracy
-        self.log('validation accuracy', self.accuracy, prog_bar = True)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters())
-
-# an LSTM classifier using a LightningModule
-class LSTM_classifier(LightningModule):
-    def __init__(self, vocab_size, embedding_dimension, state_dimension):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dimension)
-        self.lstm = nn.LSTM(
-            input_size = embedding_dimension,
-            hidden_size = state_dimension,
-            num_layers = 1, # hyperparameter
-            batch_first = True
-        )
-
-        # possible outputs: positive, negative, neutral
-        self.output = nn.Linear(state_dimension, 3)
-
-        # activation function to provide probabilities
-        self.activation = nn.Softmax(dim = 1)
-
-        # monitors accuracy
-        self.accuracy = Accuracy(task = 'multiclass', num_classes = 3)
-
-    def forward(self, sequence_batch):
-        embedded = self.embedding(sequence_batch)
-        h_t, (h_n, _) = self.lstm(embedded)  # output features (h_t) and state (h_n)
-        output = self.output(h_n[-1])
-        output = self.activation(output)
-
-        return output
-
-    def loss(self, output, targets):
-        loss = nn.CrossEntropyLoss()
-
-        return loss(output, targets)
-
-    def training_step(self, batch):
-        inputs, targets = batch
-        outputs = self.forward(inputs)
-        loss = self.loss(outputs, targets)
-
-        # get accuracy value
-        self.accuracy(outputs, targets)
-
-        # log the training accuracy
-        self.log('training accuracy', self.accuracy, prog_bar = True)
-
-        return loss
-
-    def validation_step(self, batch):
-        inputs, targets = batch
-        outputs = self.forward(inputs)
-
-        # get accuracy value
-        self.accuracy(outputs, targets)
-
-        # log the validation accuracy
-        self.log('validation accuracy', self.accuracy, prog_bar = True)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters())
-
-RNN_model = torch.load('RNN_model')
-GRU_model = torch.load('GRU_model')
-LSTM_model = torch.load('LSTM_model')
+# load & configure the LSTM model
+LSTM_model = LSTM_classifier(
+    vocab_size = len(vocab),
+    embedding_dimension = 32, # hyperparameter
+    state_dimension = 64 # hyperparameter
+)
+LSTM_model.load_state_dict(torch.load('lightning_logs/LSTM/version_0/checkpoints/epoch=19-step=26020.ckpt')['state_dict'])
+LSTM_model.eval()
 
 # create the Flask app
 app = Flask(__name__)
+CORS(app)
 
 # Receiving GET requests from the client
 @app.route('/<model_type>/<tweet_content>', methods = ['GET'])
 def return_sentiment(model_type, tweet_content):
+    
+    # tokenize the tweet_content to input it into the model
+    tokens = word_tokenize(tweet_content)
+
+    # numerically encode the tokens using the vocab object
+    content =  [vocab[token] for token in tqdm(tokens)]
+
+    # convert the encoded tokens into a torch Tensor
+    content = torch.as_tensor(content)
+
     if (model_type == 'RNN'):
-        value = RNN_model(tweet_content)
-        return Response(
-            {
-                'message': value
-            },
-            status = 200
-        )
+        value = RNN_model(torch.as_tensor(content))
+        print(value)  # display the sentiments
+        
+        return jsonify({'message': value.tolist()}), 200
     elif (model_type == 'GRU'):
-        value = GRU_model(tweet_content)
-        return Response(
-            response = {
-                'message': value
-            },
-            status = 200
-        )
+        value = GRU_model(torch.as_tensor(content))
+        print(value)  # display the sentiments
+        
+        return jsonify({'message': value.tolist()}), 200
     elif (model_type == 'LSTM'):
-        value = LSTM_model(tweet_content)
-        return Response(
-            response = {
-                'message': value
-            },
-            status = 200
-        )
+        value = LSTM_model(torch.as_tensor(content))
+        print(value)  # display the sentiments
+        
+        return jsonify({'message': value.tolist()}), 200
     else:
-        return Response(
-            response = {
-                'message': 'Model type does not exist!'
-            },
-            status = 400
-        )
+        # error message
+        return jsonify({'message': 'Model does not exist!'}), 400
 
 if __name__ == '__main__':
     app.run(debug = True)
